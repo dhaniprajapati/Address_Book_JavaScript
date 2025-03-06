de75e4a3593e65fc69977032d6f319d93fc7c6db
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-06 12:52:26 +0530

    [Dhani] Add. UC02 Validate contact details added

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -1,4 +1,4 @@
-//UC1- create an address book with contact  and addressbook class
+//UC2- ability to ensure that valid contacts are added
 class Contact {
     //constructor
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
@@ -15,35 +15,95 @@ class Contact {
 //address book class
 class AddressBook {
 
-constructor() {
-    this.contacts = [];
+    constructor() {
+        this.contacts = [];
+    }
+    //add contact
+    addContact(contact) {
+        console.log("Contact details received:", contact);
+        if (
+            validateFirstName(contact.firstName) &&
+            validateLastName(contact.lastName) &&
+            validateAddress(contact.address) &&
+            validateCity(contact.city) &&
+            validateState(contact.state) &&
+            validateZip(contact.zip) &&
+            validatePhoneNumber(contact.phoneNumber) &&
+            validateEmail(contact.email)
+        ){
+            this.contacts.push(contact);
+            console.log("Contact added successfully:", contact);
+        } else {
+            console.log("Validation failed for:");
+            if (!validateFirstName(contact.firstName)) console.log("First Name:", contact.firstName);
+            if (!validateLastName(contact.lastName)) console.log("Last Name:", contact.lastName);
+            if (!validateAddress(contact.address)) console.log("Address:", contact.address);
+            if (!validateCity(contact.city)) console.log("City:", contact.city);
+            if (!validateState(contact.state)) console.log("State:", contact.state);
+            if (!validateZip(contact.zip)) console.log("Zip Code:", contact.zip);
+            if (!validatePhoneNumber(contact.phoneNumber)) console.log("Phone Number:", contact.phoneNumber);
+            if (!validateEmail(contact.email)) console.log("Email:", contact.email);
+        }    
+    }
+    //display contacts
+    displayContacts() {
+        this.contacts.forEach((contact, index) => {
+        console.log("Contact ");
+        console.log("Name:"+contact.firstName + " " + contact.lastName);
+        console.log("Address:"+contact.address);
+        console.log("City:"+contact.city);
+        console.log("State:"+contact.state);
+        console.log("Zip:"+contact.zip);
+        console.log("Phone Number:"+contact.phoneNumber);
+        console.log("Email:"+contact.email);
+        console.log("\n");
+        });
+    }
 }
-//add contact
-addContact(contact) {
-    this.contacts.push(contact);
+//valid contact details
+function validateFirstName(firstName) {
+    return /^[A-Z][a-zA-Z]{2,}$/.test(firstName);
 }
-//display contacts
-displayContacts() {
-    this.contacts.forEach((contact, index) => {
-    console.log("Contact ");
-    console.log("Name:"+contact.firstName + " " + contact.lastName);
-    console.log("Address:"+contact.address);
-    console.log("City:"+contact.city);
-    console.log("State:"+contact.state);
-    console.log("Zip:"+contact.zip);
-    console.log("Phone Number:"+contact.phoneNumber);
-    console.log("Email:"+contact.email);
-    console.log("\n");
-    });
+
+function validateLastName(lastName) {
+return /^[A-Z][a-zA-Z]{2,}$/.test(lastName);
 }
+
+function validateAddress(address) {
+return address.length >= 4;
 }
-//create object of address book
-const addressBook = new AddressBook();
-//create object of contact
-const contact1 = new Contact("Rahul", "Kumar", "Kankarbagh", "Patna", "Bihar", 800020, 1234567890, "rahul@gmail.com");
-const contact2 = new Contact("Rohit", "Kumar", "Main road", "Ranchi", "Jharkhand", 835302, 5479434788, "rohit@gmail.com");
-//add contact to address book
-addressBook.addContact(contact1);
-addressBook.addContact(contact2);
-//display contacts
-addressBook.displayContacts();
+
+function validateCity(city) {
+return city.length >= 4;
+}
+
+function validateState(state) {
+return state.length >= 4;
+}
+
+function validateZip(zip) {
+return /^\d{5}$/.test(zip);
+}
+
+function validatePhoneNumber(phoneNumber) {
+return /^\d{10}$/.test(phoneNumber);
+}
+
+function validateEmail(email) {
+return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+}
+//try catch block
+try{
+    //create object of address book
+    const addressBook = new AddressBook();
+    //create object of contact
+    const contact1 = new Contact("Rahul", "Kumar", "Kankarbagh", "Patna", "Bihar", "80020", "1234567890", "rahul@gmail.com");
+    const contact2 = new Contact("Rohit", "Kumar", "Main road", "Ranchi", "Jharkhand", "83302", "5479434788", "rohit@gmail.com");
+    //add contact to address book
+    addressBook.addContact(contact1);
+    addressBook.addContact(contact2);
+    //display contacts
+    addressBook.displayContacts();
+} catch(error) {
+    console.error(error.message)
+}
